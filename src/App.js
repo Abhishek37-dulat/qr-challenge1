51ff20de662acb478f560bb9ade3df76a5d87912
Abhishek37-dulat <abhishek11906997dulat@gmail.com>
2023-07-14 00:40:13 +0530

    qr-challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import qr from "./assets/image-qr-code.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <div className="image">
+          <img src={qr} alt="qr-code" />
+        </div>
+        <div className="query">
+          <p style={{ fontSize: "25px", fontWeight: 700, color: "#1F3251" }}>
+            Improve your front-end
+            <br />
+            skills by building projects
+          </p>
+          <br />
+          <p style={{ fontSize: "15px", fontWeight: 400, color: "#9DA1A7" }}>
+            Scan the QR code to visit Frontend
+            <br />
+            Mentor and take your coding skills to
+            <br />
+            the next level
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
